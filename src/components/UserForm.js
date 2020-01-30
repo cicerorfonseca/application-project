0dbafc5fe060ee476b81138c950232b94ee9691a
cicerorfonseca <cicerorfonseca@gmail.com>
2020-01-29 19:31:53 -0500

    Created methods for the Next Step, Previous Step, and Input handlers

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,6 +1,38 @@
 import React, { Component } from 'react'
 
 export class UserForm extends Component {
+  state = {
+    step: 1,
+    postalCode: '',
+    firstName: '',
+    lastName: '',
+    phone: '',
+    email: '',
+    city: 'London',
+    province: 'Ontario'
+  }
+
+  //Proceed to the next step
+  nextStep = () => {
+    const { step } = this.state;
+    this.setState({
+      step: step + 1
+    })
+  }
+
+  //Go back to the previous step
+  prevStep = () => {
+    const { step } = this.state;
+    this.setState({
+      step: step - 1
+    })
+  }
+
+  //Handle fields change
+  handleChange = input => e => {
+    this.setState({ [input]: e.target.value });
+  }
+
   render() {
     return (
       <div>

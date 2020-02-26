6a6941f906a5d07d95f215ec430c4c2bb18de9e9
cicerorfonseca <cicerorfonseca@gmail.com>
2020-02-26 10:33:06 -0500

    Changed the style of the professional selection page.

--- src/RequestForm/Form05ProSelection.js
+++ src/RequestForm/Form05ProSelection.js
@@ -10,10 +10,13 @@ export class Form05ProSelection extends Component {
   //Get professionals from server
   //componentDidMount is invoked immediately after a component is mounted.
   componentDidMount() {
-    fetch('http://localhost:5000/api/professionals')
+    // fetch('http://localhost:5000/api/professionals')
+    fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
-      .then(json => this.props.updateProfessionalsList(json),
-        this.setState({ isLoaded: true }))
+      .then(json => {
+        this.props.updateProfessionalsList(json);
+        this.setState({ isLoaded: true })
+      })
   }
 
   onClickProfessional = (e) => {
@@ -23,36 +26,39 @@ export class Form05ProSelection extends Component {
 
   render() {
     var { professionalsList } = this.props;
-    console.log(professionalsList);
 
     return (
-      <div className="professionalSelection">
-        <h3>Finally, your contact details</h3>
-        <p>You are almost done, but the professional needs this information to get in touch. Only the local and relevant professionals you select in the next step will receive this information.</p>
-        <div className="card-deck">
-          {professionalsList.map(item => (
-            <div className="col-sm-6" key={item.id}>
-              <div className="card pro-card">
-                <div className="card-body">
-                  <div className="card-header">
-                    <div className="title">
-                      <input type="checkbox" class="custom-control-input" id="defaultChecked2" />
-                      <h5 className="card-title">{item.fullName}</h5>
-                      <span className="fa fa-star checked"></span>
-                      <span className="fa fa-star checked"></span>
-                      <span className="fa fa-star checked"></span>
-                      <span className="fa fa-star"></span>
-                      <span className="fa fa-star"></span>
+      <div>
+        <div className="requestForm">
+          <h3>Select the pros you'd like to send the request</h3>
+          <p>Here you can select the professionals to send your service request. Read their profile and reviews to help you choose the right service provider and these pros will contact you to discuss your job and availability.</p>
+        </div>
+        <div className="professionalSelection">
+          <div className="card-deck">
+            {professionalsList.map(item => (
+              <div className="col-sm-6" key={item.id}>
+                <div className="card pro-card">
+                  <div className="card-body">
+                    <div className="card-header">
+                      <div className="title">
+                        <input type="checkbox" className="custom-control-input" id="defaultChecked2" />
+                        <h5 className="card-title">{item.name}</h5>
+                        <span className="fa fa-star checked"></span>
+                        <span className="fa fa-star checked"></span>
+                        <span className="fa fa-star checked"></span>
+                        <span className="fa fa-star"></span>
+                        <span className="fa fa-star"></span>
+                      </div>
+                      <div className="prof-picture" id={item.id}>
+                        <img src={Logo} alt="Company Logo" id={item.id} />
+                      </div>
                     </div>
-                    <div className="prof-picture" id={item.id}>
-                      <img src={Logo} alt="Company Logo" id={item.id} />
-                    </div>
+                    <p className="card-text" id={item.id}>I am a plumber,and I install and repair pipes that supply water and gas to, as well as carry waste away from, homes and businesses. They also install plumbing fixtures such as bathtubs, sinks, and toilets.</p>
                   </div>
-                  <p className="card-text" id={item.id}>I am a plumber,and I install and repair pipes that supply water and gas to, as well as carry waste away from, homes and businesses. They also install plumbing fixtures such as bathtubs, sinks, and toilets.</p>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
     )
